ab6ac1fb1de7840b54cce4194e7857e741083596
Wade Hunter <wade.hunter87@gmail.com>
2021-03-08 12:06:32 -0700

    R3F Next config build test

--- web/components/Three/Skull/index.js
+++ web/components/Three/Skull/index.js
@@ -1,10 +1,15 @@
-import React, { useMemo } from "react"
-import { useLoader, useFrame } from "react-three-fiber"
+import React, { useMemo, useState, useEffect } from "react"
+import { useFrame } from "react-three-fiber"
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader"
 import gsap from "gsap"
 import { frag, vert } from "../Shaders/skull"
 import * as THREE from "three"
 
 const Skull = ({ isTablet, isMobile, pointer, scroll }) => {
+    // State
+    const [skullObject, setSkullObject] = useState()
+
+    // Uniforms
     const uniforms = useMemo(
         () => ({
             uTime: { value: 0.0 },
@@ -35,6 +40,27 @@ const Skull = ({ isTablet, isMobile, pointer, scroll }) => {
 
     // const skullObject = getObject()
 
+    useEffect(() => {
+        const loader = new OBJLoader()
+        loader.load("/assets/skull.obj", async obj => {
+            console.log(obj)
+
+            obj.scale.set(0.3, 0.3, 0.3)
+
+            const material = new THREE.ShaderMaterial({
+                uniforms: uniforms,
+                vertexShader: vert,
+                fragmentShader: frag,
+            })
+
+            for (let i = 0; i < obj.children.length; i++) {
+                obj.children[i].material = material
+            }
+
+            setSkullObject(obj)
+        })
+    }, [])
+
     // Mouse animations
     const animateX = e => {
         gsap.to(e, {
@@ -60,40 +86,41 @@ const Skull = ({ isTablet, isMobile, pointer, scroll }) => {
     })
 
     // RAF
-    // useFrame((state, delta) => {
-    //     uniforms.uTime.value += delta
+    useFrame((state, delta) => {
+        uniforms.uTime.value += delta
 
-    //     // Responsive
+        if (skullObject) {
+            // Responsive
+            if (isMobile) {
+                skullObject.scale.set(0.15, 0.15, 0.15)
+                skullObject.position.y = -1.8
+            } else if (isTablet) {
+                skullObject.scale.set(0.2, 0.2, 0.2)
+                skullObject.position.y = -2.2
+            } else {
+                skullObject.scale.set(0.3, 0.3, 0.3)
+                skullObject.position.y = -3.5
+            }
 
-    //     if (isMobile) {
-    //         skullObject.scale.set(0.15, 0.15, 0.15)
-    //         skullObject.position.y = -1.8
-    //     } else if (isTablet) {
-    //         skullObject.scale.set(0.2, 0.2, 0.2)
-    //         skullObject.position.y = -2.2
-    //     } else {
-    //         skullObject.scale.set(0.3, 0.3, 0.3)
-    //         skullObject.position.y = -3.5
-    //     }
+            // Position/rotation
+            skullObject.position.y += scrollY / 150
+            skullObject.position.y += Math.sin(uniforms.uTime.value / 1.75) / 2
+            skullObject.rotation.x = Math.PI / 0.63
+            skullObject.rotation.z = uniforms.uMouse.value.x / 10
+            skullObject.rotation.x += uniforms.uMouse.value.y / -10
+        }
 
-    //     // Position/rotation
-    //     skullObject.position.y += scrollY / 150
-    //     skullObject.position.y += Math.sin(uniforms.uTime.value / 1.75) / 2
-    //     skullObject.rotation.x = Math.PI / 0.63
-    //     skullObject.rotation.z = uniforms.uMouse.value.x / 10
-    //     skullObject.rotation.x += uniforms.uMouse.value.y / -10
+        // Mouse animations
+        animateX(uniforms.uMouse.value)
+        animateY(uniforms.uMouse.value)
+    })
 
-    //     // Mouse animations
-    //     animateX(uniforms.uMouse.value)
-    //     animateY(uniforms.uMouse.value)
-    // })
-
-    // return <primitive position={[0, -3.5, -15]} object={skullObject} />
     return (
-        <mesh>
-            <boxBufferGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+        <>
+            {skullObject ? (
+                <primitive position={[0, -3.5, -15]} object={skullObject} />
+            ) : null}
+        </>
     )
 }
 
--- web/next.config.js
+++ web/next.config.js
@@ -1,10 +1,114 @@
 // next.config.js
-module.exports = {
-    images: {
-        domains: ["res.cloudinary.com"],
-    },
+// module.exports = {
+//     images: {
+//         domains: ["res.cloudinary.com"],
+//     },
+// }
+
+// const withTM = require("next-transpile-modules")(["@react-three/drei", "three"])
+
+// module.exports = withTM()
+
+// R3F starter
+const plugins = require("next-compose-plugins")
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true",
+})
+
+// custom transpile module
+const debug = false
+let start = null
+let i = 0
+
+const match = path => {
+    if (!path.includes("three/examples/jsm")) return false
+    if (debug) {
+        // should be around 3/4 seconds the first time and then 200ms after using Webpack 5 built-in loader cache
+        let end = start ? new Date() - start : 0
+        console.log(
+            `\x1b[37m`,
+            `🚄 ${end}ms - The transpilation ${path} in process`
+        )
+        if (i === 1) {
+            start = new Date()
+        }
+        i++
+    }
+    return true
 }
 
-const withTM = require("next-transpile-modules")(["@react-three/drei", "three"])
+const withOffline = require("next-offline")
+const withTM = require("next-transpile-modules")(
+    ["three", "react-three-fiber", "@react-three/drei"], // '@react-three/postprocessing'
+    { debug: debug, __unstable_matcher: match } // symlink-caused loops which cause memory to get bloated exponentially.
+)
 
-module.exports = withTM()
+// the config break if we use next export
+const nextConfig =
+    process.env.EXPORT !== "true"
+        ? {
+              images: {
+                  domains: ["res.cloudinary.com"],
+              },
+              future: {
+                  webpack5: true,
+              },
+              webpack(config) {
+                  config.module.rules.push(
+                      { test: /react-spring/, sideEffects: true }, // prevent vercel to crash when deploy
+                      {
+                          test: /\.(glsl|vs|fs|vert|frag)$/,
+                          exclude: /node_modules/,
+                          use: ["raw-loader", "glslify-loader"],
+                      }
+                  )
+                  return config
+              },
+          }
+        : {}
+
+// manage i18n
+if (process.env.EXPORT !== "true") {
+    nextConfig.i18n = {
+        locales: ["en-US"],
+        defaultLocale: "en-US",
+    }
+}
+
+module.exports = plugins(
+    [
+        withTM(nextConfig),
+        [
+            withOffline,
+            {
+                workboxOpts: {
+                    swDest: process.env.NEXT_EXPORT
+                        ? "service-worker.js"
+                        : "static/service-worker.js",
+                    runtimeCaching: [
+                        {
+                            urlPattern: /^https?.*/,
+                            handler: "NetworkFirst",
+                            options: {
+                                cacheName: "offlineCache",
+                                expiration: {
+                                    maxEntries: 200,
+                                },
+                            },
+                        },
+                    ],
+                },
+                async rewrites() {
+                    return [
+                        {
+                            source: "/service-worker.js",
+                            destination: "/_next/static/service-worker.js",
+                        },
+                    ]
+                },
+            },
+        ],
+        withBundleAnalyzer,
+    ],
+    nextConfig
+)
