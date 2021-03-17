19e121293983ee07cca7fa8283c5863204ad52ed
Wade Hunter <wade.hunter87@gmail.com>
2021-03-17 16:48:35 -0600

    Bio update

--- web/pages/bio.js
+++ web/pages/bio.js
@@ -23,17 +23,18 @@ const Bio = props => {
                 this bizarre timeline we’ve all been chosen to exist in. Blazing
                 dual guitars, thundering bass, intricate percussion and lyrics
                 that would make Charlemagne second guess himself. With a
-                dynamite rhythm section comprised of Farmer on drums, and Puff
-                Chaddy on bass, they are the key components that keep the engine
-                running. In the passenger seat is Reese Richards, whose twangy
-                telecaster licks and sweet vocal harmonies are the seasoning
-                that leave you wanting more. Last but not least, the one at the
-                helm, on lead vocals and guitar is Reverend Kitchener Langfield
-                III. In 2019 he left the bustling metropolis of Vancouver for
-                the prairie skies of Southwest Saskatchewan on a divine calling
-                to start the world's greatest rock and roll band. And now ladies
-                and gentlemen, that dream has become a reality. So turn it up,
-                shout it out and give it up for the band goddamnit!
+                dynamite rhythm section comprised of F@RM3R on drums, and Chad
+                "Puff Chaddy" Krause on bass, they are the key components that
+                keep the engine running. In the passenger seat is Nick "Reese
+                Richards" Gauvin, whose twangy telecaster licks and sweet vocal
+                harmonies are the seasoning that leave you wanting more. Last
+                but not least, the one at the helm, on lead vocals and guitar is
+                Reverend Kitchener Langfield III. In 2019 he left the bustling
+                metropolis of Vancouver for the prairie skies of Southwest
+                Saskatchewan on a divine calling to start the world's greatest
+                rock and roll band. And now ladies and gentlemen, that dream has
+                become a reality. So turn it up, shout it out and give it up for
+                the band goddamnit!
             </p>
             <p>We are Cadillac, and we’re here to play rock n roll.</p>
             <div ref={ref} className={styles.bioImage}></div>
